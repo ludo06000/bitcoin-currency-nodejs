ccf292afb13b1aa7474f65d3649ff5869e679881
ludo06000 <lc06000@gmail.com>
2023-01-21 07:31:08 +0100

    update readMe

--- btc.js
+++ btc.js
@@ -1,27 +1,32 @@
-
 const axios = require('axios');
 
+// Declare async function to get Bitcoin exchange rate
 async function getBitcoint() {
-    
+    // Get the currency specified as a command line argument, default to EUR
     const currency = process.argv[2] 
         ? process.argv[2].toUpperCase() 
         : "EUR";
 
     try{
+        // Endpoint for CoinDesk API
         const endpoint = 'https://api.coindesk.com/v1/bpi/currentprice.json'
+        // Make a GET request to the API
         const { data } = await axios.get(endpoint)
 
+        // Check if the requested currency is supported by the API
         if (!data.bpi[currency]) {
             throw new Error('Devise inconnue only EUR / USD /GBP')
         }
+        // Get the exchange rate and last update time from the API
         const rate = data.bpi[currency].rate;
         const updatedTime = data.time.updated;
+        // Log the exchange rate and time to the console
         console.log(`> 1 BITCOIN (BTC) = ${rate} ${currency} (Time : ${updatedTime})`);
     }catch(error){
+        // Log any errors to the console
         console.error(error.toString());
     }
 }
 
+// Call the function
 getBitcoint()
-
-
--- readMe.md
+++ readMe.md
@@ -1,4 +1,20 @@
-use API coindesk
+# Node Js - recovers the bitcoin price
 
-install nodemon
-install axios use async await
+## SUMMARY
+The code in the btc.js file uses the axios library to make a request to a site API (https://www.coindesk.com) that returns the current bitcoin exchange rate based on the requested currency. If no currency is specified as a command line argument, the default currency is the Euro. The code also checks if the requested currency is supported by the API, and raises an error if it is not. Finally, the code displays the exchange rate and the time of the last API update.
+
+## USE
+
+### Initialisation of project
+
+install dependecies
+```sh
+npm install
+```
+
+### Dependencies
+
+| Dependencies             | Documentation                                                                |
+| ----------------- | ------------------------------------------------------------------ |
+| nodemon | npm i -D nodemon (https://www.npmjs.com/package/nodemon) |
+| axios | npm i axios (https://www.npmjs.com/package/axios) |
